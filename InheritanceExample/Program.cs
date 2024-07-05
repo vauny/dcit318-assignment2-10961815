1d86152b20ad730d649710246c72da9c8d60a87d
vauny <vndhammond001@st.ug.edu.gh>
2024-07-05 17:33:17 +0000

    Added inheritance and method overriding example

---
+++ InheritanceExample/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more inf
+
+
+using System;
+
+namespace InheritanceExample
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }    
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Animal genericAnimal = new Animal();
+        genericAnimal.MakeSound();
+
+        Dog dog = new Dog();
+        dog.MakeSound();
+
+        Cat cat = new Cat();
+        cat.MakeSound();
+        }
+    }
+    
+}
