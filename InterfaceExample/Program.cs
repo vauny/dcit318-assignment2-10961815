455d827018f97b771b13898fa26794775cade3d5
vauny <vndhammond001@st.ug.edu.gh>
2024-07-05 17:56:17 +0000

    Added interfaces example

---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace InterfaceExample
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            car.Move();
+
+            IMovable bicycle = new Bicycle();
+            bicycle.Move();
+        }
+    }
+}
+
